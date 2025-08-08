0d874c60af393b6ac03ae908865a562bc8d6706b
Kelvinn03 <KelvinGoh48@gmail.com>
2025-08-08 16:14:27 +0800

    Initial Commit

---
+++ Trip Split Bill/Trip_Split_BillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trip_Split_BillApp.swift
+//  Trip Split Bill
+//
+//  Created by Kelvin on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Trip_Split_BillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Trip Split BillTests/Trip_Split_BillTests.swift
@@ -0,0 +1,17 @@
+//
+//  Trip_Split_BillTests.swift
+//  Trip Split BillTests
+//
+//  Created by Kelvin on 08/08/25.
+//
+
+import Testing
+@testable import Trip_Split_Bill
+
+struct Trip_Split_BillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
